6f5c3856dea01e94ec60c38b1801f64d16e369f2
anil <anil.kumar@antiersolutions.com>
2022-10-07 12:09:33 +0530

    layout create

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -2,67 +2,76 @@ import React from "react";
 import { Container, Row, Col, InputGroup, Form, Button } from "react-bootstrap";
 import logo from "../../Image/logo.svg";
 import { Link } from "react-router-dom";
+import linkedIn from "../../Image/linkedin.svg";
+import twitter from "../../Image/twitter.svg";
+import insta from "../../Image/instagram.svg";
+import "./Footer.scss";
+import Footerbar from "../footerbar/Footerbar";
 
-function Footer() {
+function Footer({ className }) {
   return (
     <>
-      <section>
+      <section className={`footerSec ${className}`}>
         <Container>
           <footer>
             <Row>
               <Col>
-                <img src={logo} alt="logo" />
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+                <div className="firstCol">
+                  <img src={logo} alt="logo" />
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
               </Col>
               <Col>
-                <h6>Quick Link</h6>
-                <ul>
-                  <li>
-                    <span>Home</span>
-                    <span>Tokenomics</span>
-                  </li>
-                </ul>
-                <ul>
-                  <li>
-                    <span>Why Us</span>
-                    <span>Roadmap</span>
-                  </li>
-                </ul>
-                <ul>
-                  <li>
-                    <span> Solutions</span>
-                    <span>Blogs</span>
-                  </li>
-                </ul>
+                <div className="secCol">
+                  <h6>Quick Link</h6>
+                  <ul>
+                    <li>
+                      <span>Home</span>
+                      <span>Tokenomics</span>
+                    </li>
+                    <li>
+                      <span>Why Us</span>
+                      <span>Roadmap</span>
+                    </li>
+                    <li>
+                      <span> Solutions</span>
+                      <span>Blogs</span>
+                    </li>
+                  </ul>
+                </div>
               </Col>
               <Col>
-                <InputGroup className="mb-3">
-                  <Form.Control
-                    placeholder="Recipient's username"
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                  />
-                  <Button variant="outline-secondary" id="button-addon2">
-                    Button
-                  </Button>
-                </InputGroup>
-                <h6>Social Media Links</h6>
-                <Link to="">
-                  <img src="" alt="" />
-                </Link>
-                <Link to="">
-                  <img src="" alt="" />
-                </Link>
-                <Link to="">
-                  <img src="" alt="" />
-                </Link>
+                <div className="thirdSec">
+                  <InputGroup>
+                    <Form.Control
+                      placeholder="Enter Email ID"
+                      aria-label="Recipient's username"
+                      aria-describedby="basic-addon2"
+                    />
+                    <Button variant="outline-secondary" id="button-addon2">
+                      Button
+                    </Button>
+                  </InputGroup>
+                  <h6>Social Media Links</h6>
+                  <Link to="">
+                    <img src={linkedIn} alt="" />
+                  </Link>
+                  <Link to="">
+                    <img src={twitter} alt="" />
+                  </Link>
+                  <Link to="">
+                    <img src={insta} alt="" />
+                  </Link>
+                </div>
               </Col>
             </Row>
           </footer>
         </Container>
+        <Footerbar />
       </section>
     </>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,28 +2,31 @@ import React from "react";
 import Footer from "../../component/footer/Footer";
 import Footerbar from "../../component/footerbar/Footerbar";
 import Header from "../../component/header/Header";
+import Layout from "../../component/layout/Layout";
 import "./Home.scss";
 
 function Home() {
   return (
     <div>
-      <Header />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, commodi.
-      Quos adipisci animi voluptate tenetur sapiente praesentium consequatur
-      corrupti reiciendis in et exercitationem consequuntur maxime dignissimos
-      excepturi placeat voluptatum sequi quod fugit pariatur modi, aspernatur
-      soluta voluptatibus consectetur saepe! Molestiae, esse quam unde labore
-      iusto facere et, doloribus debitis quae cum adipisci quia quaerat
-      officiis, sunt eius omnis ducimus. Rem a ducimus provident deleniti
-      exercitationem, quae impedit numquam quo corrupti voluptatum nostrum
-      cupiditate asperiores ea? Possimus quis, impedit dicta iure minima sed
-      esse quam saepe consectetur voluptatibus quasi harum delectus explicabo ex
-      natus maxime ut sapiente aperiam eligendi fuga repellat ab aliquid? Ea,
-      dolores! Commodi beatae aliquid assumenda, dolorem numquam amet illum quas
-      quisquam minima saepe placeat possimus fugit aut. hellow how are you
-      <Footer />
-      <Footerbar />
-      welcome
+      <Layout>
+        {/* <Header /> */}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+        commodi. Quos adipisci animi voluptate tenetur sapiente praesentium
+        consequatur corrupti reiciendis in et exercitationem consequuntur maxime
+        dignissimos excepturi placeat voluptatum sequi quod fugit pariatur modi,
+        aspernatur soluta voluptatibus consectetur saepe! Molestiae, esse quam
+        unde labore iusto facere et, doloribus debitis quae cum adipisci quia
+        quaerat officiis, sunt eius omnis ducimus. Rem a ducimus provident
+        deleniti exercitationem, quae impedit numquam quo corrupti voluptatum
+        nostrum cupiditate asperiores ea? Possimus quis, impedit dicta iure
+        minima sed esse quam saepe consectetur voluptatibus quasi harum delectus
+        explicabo ex natus maxime ut sapiente aperiam eligendi fuga repellat ab
+        aliquid? Ea, dolores! Commodi beatae aliquid assumenda, dolorem numquam
+        amet illum quas quisquam minima saepe placeat possimus fugit aut. hellow
+        how are you
+        {/* <Footer />
+      <Footerbar /> */}
+      </Layout>
     </div>
   );
 }
