6864e487917fbe512ee3c0072d25aa59715b9f36
anil <anil.kumar@antiersolutions.com>
2023-01-02 19:06:51 +0530

    please add changes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./pages/About/About";
 import List from "./pages/listpage/List";
 import StyleComlponent from "./pages/StyleComponent/StyleComlponent";
+import Auth from "./pages/auth/Auth";
+import Accodian from "./pages/accodian/Accodian";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/auth" element={<Auth />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/style" element={<StyleComlponent />} />
+        <Route path="/Accodian" element={<Accodian />} />
       </Routes>
     </div>
   );
